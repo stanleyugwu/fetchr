ef15da28576d2dc6c9d6eab3e84d0cedf5dd11df
Stanley Ugwu <stanleyugwu2018@gmail.com>
2020-11-14 23:48:24 +0100

    heroku deployable

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/Footer.css';
+export default function Footer(props){
+    return(
+        <footer className="footer">
+            <p className="devvie">Made With <span className="mdi mdi-heart"></span> By Stanley</p>
+        </footer>
+    )
+}
