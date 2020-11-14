50e38e31ee9ac80a66c735dceff0b63fe2573e4a
Stanley Ugwu <stanleyugwu2018@gmail.com>
2020-11-14 23:11:45 +0100

    undo tracking

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../styles/Footer.css';
-export default function Footer(props){
-    return(
-        <footer className="footer">
-            <p className="devvie">Made With <span className="mdi mdi-heart"></span> By Stanley</p>
-        </footer>
-    )
-}
